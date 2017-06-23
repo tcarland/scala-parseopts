af8b04ca0c6c2392746e00f1253c1d29245394d1
Timothy C. Arland <tcarland@gmail.com>
2017-06-23 16:43:50 -0700

    show args pre-parse in test

--- bin/cmdopt_test.sh
+++ bin/cmdopt_test.sh
@@ -7,13 +7,7 @@ CMDOPTS_CLASS="com.trace3.spark.examples.OptionTest"
 CMDOPTS_JAR="target/spark-parseopts-0.1.1.jar"
 
 
-spark-submit --master yarn \
-  --deploy-mode client \
-  --num-executors 4 \
-  --executor-memory 1g \
-  --class $CMDOPTS_CLASS \
-  $CMDOPTS_JAR \
-  $@
+scala -classpath $CMDOPTS_JAR $CMDOPTS_CLASS $@
 
 #eg.  --input somefile.txt --table myTable 50 51 52
  
--- src/main/scala/com/trace3/spark/examples/OptionTest.scala
+++ src/main/scala/com/trace3/spark/examples/OptionTest.scala
@@ -3,8 +3,6 @@
   */
 package com.trace3.spark.examples
 
-
-import org.apache.spark.sql.SparkSession
 import com.trace3.spark.util.ParseOpts
 
 
@@ -18,25 +16,26 @@ object OptionTest {
 
 
   def main ( args: Array[String] ) : Unit = {
-    val spark = SparkSession.builder()
-      .appName("OptionTest")
-      .getOrCreate()
-
     val (optMap, optList) = ParseOpts.parseOpts(args.toList)
+
+    println("Args:")
+    args.foreach { x => println(" => '" + x + "'")}
+    println("")
 
     val fileName  = optMap.getOrElse("input", None)
     val tableName = optMap.getOrElse("table", None)
 
-    println(" ===>  Expected Options: ")
-    println(" ===>  fileName = " + fileName)
-    println(" ===>  table = " + tableName)
+    println(" ==>  Expected Options: ")
+    println(" ==>  fileName = " + fileName)
+    println(" ==>  table = " + tableName)
 
-    println(" ===> OptionMap:")
+    println(" ==> OptionMap:")
     optMap.foreach { x => println(" ===>    " + x._1 + " -> " + x._2) }
 
+    println(" --> OptionList:")
     if ( optList.length < 3 )
-      println(" ===>  missing args (<3)")
+      println(" -->  missing args (<3)")
 
-    optList.foreach { x => println(" ===>    " + x) }
+    optList.foreach { x => println(" -->    " + x) }
   }
 }
