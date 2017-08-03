47d4e57c8d682cf3d163ee0e15e015d61c49821d
Timothy C. Arland <tcarland@gmail.com>
2017-08-02 21:38:29 -0700

    remove unneeded main from module

--- src/main/scala/com/trace3/util/ParseOpts.scala
+++ src/main/scala/com/trace3/util/ParseOpts.scala
@@ -36,27 +36,6 @@ object ParseOpts {
     nextOpt(args, Map())
   }
 
-
-  def usage =
-    """
-      |ParseOpts <args>
-      |
-    """.stripMargin
-
-
-  def main ( args: Array[String] ) : Unit = {
-    val (optMap, optList) = ParseOpts.parseOpts(args.toList)
-
-    println("Args: " + args.length.toString)
-    args.foreach { x => println(" => '" + x + "'")}
-    println("")
-
-    println(" ==> OptionMap:" + optMap.size.toString)
-    optMap.foreach { x => println(" ===>    " + x._1 + " -> " + x._2) }
-
-    println(" --> OptionList: " + optList.length.toString)
-    optList.foreach { x => println(" -->    " + x) }
-  }
 }
 
 
---
+++ src/test/scala/ParseOptsTest.scala
@@ -0,0 +1,26 @@
+import com.trace3.util.ParseOpts
+
+object ParseOptsTest {
+  
+  def usage =
+    """
+      |ParseOptsTest <args>
+      |
+    """.stripMargin
+
+
+  def main ( args: Array[String] ) : Unit = {
+    val (optMap, optList) = ParseOpts.parseOpts(args.toList)
+
+    println("Args: " + args.length.toString)
+    args.foreach { x => println(" => '" + x + "'")}
+    println("")
+
+    println(" ==> OptionMap:" + optMap.size.toString)
+    optMap.foreach { x => println(" ===>    " + x._1 + " -> " + x._2) }
+
+    println(" --> OptionList: " + optList.length.toString)
+    optList.foreach { x => println(" -->    " + x) }
+  }
+
+}
